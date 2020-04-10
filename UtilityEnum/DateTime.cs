5e12e011703ca407ebcd0089d704e9a97b006d98
dtaylor-530 <d.taylor530@gmail.com>
2020-04-10 17:15:13 +0100

    Added DateTime and TimeInterval

---
+++ UtilityEnum/DateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityEnum
+{
+    public enum DateTime
+    {
+        None,
+        Date=1,
+        Time,
+        DateTime
+    }
+}
---
+++ UtilityEnum/TimeInterval.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityEnum
+{
+    public enum TimeInterval
+    {
+        None,
+        MilliSecond = 1,
+        Second,
+        Minute,
+        Hour,
+        Day,
+        Week,
+        Month,
+        Year,
+
+    }
+
+
+}
