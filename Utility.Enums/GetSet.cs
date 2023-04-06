7f1295437681cc650ead27ec837a5deed1a249eb
dtaylor-530 <d.taylor530@gmail.com>
2023-04-06 12:03:24 +0100

    Adds GetSet

---
+++ Utility.Enums/GetSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Enums
+{
+    public enum GetSet
+    {
+        Nonde, 
+        Get = 1,
+        Set
+    }
+}
