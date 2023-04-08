aa13494aa7c875d338526a55e8fa90c49cae2653
dtaylor-530 <d.taylor530@gmail.com>
2023-04-08 12:58:21 +0100

    Adds Step enum

---
+++ Utility.Enums/Step.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Enums
+{
+    [Flags]
+
+    public enum Step
+    {
+        None,
+        Backward = 1,
+        Forward = 2,
+        Walk = 4,
+        Wait = 8,
+        All = Backward | Forward | Walk | Wait
+
+    }
+}
