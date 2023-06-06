730994e31ed751fe8babb339714f1697a19390b7
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-06-06 23:14:36 +0100

    Adds Position2D, Adds Position3D, Adds to Arrangement

---
+++ Utility.Enums/Position2D.cs
@@ -0,0 +1,17 @@
+﻿namespace Utility.Enums
+{
+    /// <summary>
+    /// <a href="https://en.wikipedia.org/wiki/Relative_direction"></a>
+    /// relative direction
+    /// </summary>
+    [System.Flags]
+    public enum Position2D : byte
+    {
+        None = 0,
+        Left = 1,
+        Right = 2,     
+        Top = 16,
+        Bottom = 32,
+        All = Left | Right | Top | Bottom
+    }
+}
