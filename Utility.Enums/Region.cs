d3655a02b7fd5c3a993f81f04e7710085f794979
dtaylor-530 <d.taylor530@gmail.com>
2026-08-07 15:28:10 +0100

    Utility.Enums Improves Progress and Region

--- Utility.Enums/Region.cs
+++ Utility.Enums/Region.cs
@@ -1,16 +1,28 @@
-﻿namespace Utility.Enums
+namespace Utility.Enums
 {
-    public enum Region : byte
+    [Flags]
+    public enum Region : short
     {
-        None,
-        TopLeft = 1,
-        Top,
-        TopRight,
-        Right,
-        BottomRight,
-        Bottom,
-        BottomLeft,
-        Left,
-        Middle
+        None = 0,
+
+        TopLeft = 1 << 0,
+        Top = 1 << 1,
+        TopRight = 1 << 2,
+        Right = 1 << 3,
+        BottomRight = 1 << 4,
+        Bottom = 1 << 5,
+        BottomLeft = 1 << 6,
+        Left = 1 << 7,
+        Middle = 1 << 8,
+
+        TopRow = TopLeft | Top | TopRight,
+        BottomRow = BottomLeft | Bottom | BottomRight,
+        LeftColumn = TopLeft | Left | BottomLeft,
+        RightColumn = TopRight | Right | BottomRight,
+
+        Horizontal = Left | Right,
+        Vertical = Top | Bottom,
+
+        All = TopRow | BottomRow | Left | Right | Middle
     }
 }
