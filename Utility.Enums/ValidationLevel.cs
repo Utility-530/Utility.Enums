4b56b523c6ad0632ecc5d21eb0696ce9bec68a2f
dtaylor-530 <d.taylor530@gmail.com>
2026-08-30 10:47:24 +0100

    Utility.Enums - ValidationLevel -- Updates

---
+++ Utility.Enums/ValidationLevel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Enums;
+
+public enum ValidationLevel
+{
+    Valid,
+    Unvalidated,
+    Warning,
+    Invalid
+}
