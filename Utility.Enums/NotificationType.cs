c4489ba6d8f71bff9090ccd6ec4b0bd4c5a9e7f3
dtaylor-530 <d.taylor530@gmail.com>
2026-09-03 16:05:44 +0100

    Utility.Enums - NotificationType -- Adds

---
+++ Utility.Enums/NotificationType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Enums;
+
+public enum NotificationType
+{
+    None,
+    Processing,
+    Success,
+    Info,
+    Warning,
+    Error,
+}
